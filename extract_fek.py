3453860cb2566a9f623ca73c4239040084ee038d
Fabian <32894026+fabianhoegger@users.noreply.github.com>
2023-04-08 18:42:19 +0300

    pdf downloading successfull

--- extract_fek.py
+++ extract_fek.py
@@ -3,42 +3,52 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome("C:/Users/30697/trialenviroment/chromedriver_win/chromedriver.exe")
-driver.get("https://www.et.gr/SearchFekLektiko")
-title = driver.title
-print("title :",title)
-driver.implicitly_wait(13)
+import time
+def init_driver():
+    #load Driver
+    driver = webdriver.Chrome("C:/Users/30697/trialenviroment/chromedriver_win/chromedriver.exe")
+    #give link to detailed search
+    driver.get("https://www.et.gr/SearchFekLektiko")
+    #wait for website to load
+    driver.implicitly_wait(13)
+    return driver
+def search(driver):
+    #find dropdown and select by clicking it
+    dropdown= driver.find_element(By.CLASS_NAME, "dx-dropdowneditor-input-wrapper")
+    driver.implicitly_wait(10)
+    dropdown.click()
+    EmbeddedDataGridSingle = driver.find_element_by_id("EmbeddedDataGridSingle")
+    # Find all rows
+    rows = driver.find_elements(By.CLASS_NAME ,"dx-row")
+    for row in rows:
+        # find row of dropdown
+        if row.text=="2023 Α":
+            row.click()
+    #find all list of elements to narrow down to input field
+    rows = driver.find_elements(By.CLASS_NAME ,"dx-texteditor-input")
+    for row in rows:
+            if "30"==row.get_attribute('maxlength'):
+                #write to input
+                row.send_keys("ΑΣΕΠ")
+                driver.implicitly_wait(1)
+                #press enter for website to pull the results
+                row.send_keys(Keys.ENTER)
+    driver.implicitly_wait(5)
+    return driver
+def select_pdf(driver):
+    time.sleep(15)
 
-dropdown= driver.find_element(By.CLASS_NAME, "dx-dropdowneditor-input-wrapper")
-driver.implicitly_wait(10)
-dropdown.click()
+    rows = driver.find_elements_by_tag_name("td")
+    print(len(rows))
+    for row in rows:
+        if(row.get_attribute("aria-describedby")=="dx-col-11"):
+            time.sleep(1)
+            row.click()
+            time.sleep(10)
+            break
 
-EmbeddedDataGridSingle = driver.find_element_by_id("EmbeddedDataGridSingle")
 
-# Find the first row of the datagrid
-rows = driver.find_elements(By.CLASS_NAME ,"dx-row")
-for row in rows:
-    if row.text=="2023 Α":
-        row.click()
 
-rows = driver.find_elements(By.CLASS_NAME ,"dx-texteditor-input")
-for row in rows:
-        if "30"==row.get_attribute('maxlength'):
-            row.send_keys("ΑΣΕΠ")
-            driver.implicitly_wait(1)
-            row.send_keys(Keys.ENTER)
-driver.implicitly_wait(5)
-button = driver.find_element(By.CLASS_NAME ,"dx-button-content")
-print(button.value_of_css_property())
-
-"""
-dx-texteditor-input
-
-_id("dataGridContainer")
-id="Search "
-dx-scrollable-wrapper
-dx-scrollable-container
-dx-datagrid-content
-dx-datagrid-table dx-datagrid-table-fixed
-dx-texteditor-input-container
-"""
+driver=init_driver()
+driver=search(driver)
+select_pdf(driver)
