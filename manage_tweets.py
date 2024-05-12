012a80976566b247613d04ad3a5619ff1ff359c4
fabianhogger <draizfabian@gmail.com>
2024-05-12 20:22:34 +0300

    setters & tweet update

--- manage_tweets.py
+++ manage_tweets.py
@@ -2,6 +2,7 @@ import tweepy
 import json
 from Summerize import Summerize
 from Fek_getter import Fek_getter
+
 f = open("secret_params.json",)
 parameters=json.load(f)
 
@@ -25,10 +26,16 @@ client = tweepy.Client(consumer_key=consumer_key,
 fek_object=Fek_getter()
 pdf_obj=fek_object.get_fek()
 input=pdf_obj[0]
+pdf_obj.teyxos='2089'
+pdf_obj.fullo='B'
+
 sum=Summerize()
 jsonobj=sum.summerize(input)
 print(jsonobj)
 print(type(jsonobj))
 diction=json.loads(jsonobj)
 print(diction['output'])
-response = client.create_tweet(text=diction['output'])
+if(diction['output']):
+    response = client.create_tweet(text=diction['output']+' Τεύχος: '+('not' or pdf_obj.teyxos)+' Φύλλο: '+('not found ' or pdf_obj.fullo))
+else:
+    print('not tweeted')
