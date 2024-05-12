6ed608fa5fad302c49d9aa0554bbca327ea48ea7
fabianhogger <draizfabian@gmail.com>
2024-05-12 20:59:46 +0300

    trivial class

--- manage_tweets.py
+++ manage_tweets.py
@@ -7,20 +7,20 @@ f = open("secret_params.json",)
 parameters=json.load(f)
 
 
+class Tweeter_post:
+    def __init__(self):
+    # Define your Twitter API credentials
+    self.consumer_key = parameters["consumer_key"]
+    self.consumer_secret =  parameters["consumer_key_secret"]
+    self.access_token =  parameters["access_token"]
+    self.access_token_secret =  parameters["access_token_secret"]
 
 
-# Define your Twitter API credentials
-consumer_key = parameters["consumer_key"]
-consumer_secret =  parameters["consumer_key_secret"]
-access_token =  parameters["access_token"]
-access_token_secret =  parameters["access_token_secret"]
-
-
-
-client = tweepy.Client(consumer_key=consumer_key,
-                       consumer_secret=consumer_secret,
-                       access_token=access_token,
-                       access_token_secret=access_token_secret)
+post=Tweeter_post()
+client = tweepy.Client(consumer_key=post.consumer_key,
+                       consumer_secret=post.consumer_secret,
+                       access_token=post.access_token,
+                       access_token_secret=post.access_token_secret)
 # Replace the text with whatever you want to Tweet about
 
 fek_object=Fek_getter()
