9561f3b292de314d083f2f0c9bcc9f2a1e49af91
Fabian <32894026+fabianhogger@users.noreply.github.com>
2024-05-13 20:46:54 +0300

    Feat tweeter1 (#22)
    * setters & tweet update
    
    * schedule run
    
    * trivial class
    
    * trivial class

--- manage_tweets.py
+++ manage_tweets.py
@@ -7,25 +7,25 @@ f = open("secret_params.json",)
 parameters=json.load(f)
 
 
+class Tweeter_post:
+    def __init__(self):
+    # Define your Twitter API credentials
+        self.consumer_key = parameters["consumer_key"]
+        self.consumer_secret =  parameters["consumer_key_secret"]
+        self.access_token =  parameters["access_token"]
+        self.access_token_secret =  parameters["access_token_secret"]
 
 
-# Define your Twitter API credentials
-consumer_key = parameters["consumer_key"]
-consumer_secret =  parameters["consumer_key_secret"]
-access_token =  parameters["access_token"]
-access_token_secret =  parameters["access_token_secret"]
-
-
-
-client = tweepy.Client(consumer_key=consumer_key,
-                       consumer_secret=consumer_secret,
-                       access_token=access_token,
-                       access_token_secret=access_token_secret)
+post=Tweeter_post()
+client = tweepy.Client(consumer_key=post.consumer_key,
+                       consumer_secret=post.consumer_secret,
+                       access_token=post.access_token,
+                       access_token_secret=post.access_token_secret)
 # Replace the text with whatever you want to Tweet about
 
 
 fek_object=Fek_getter()
-pdf_obj=fek_object.get_fek()
+pdf_obj=fek_object.get_latest_fek()
 input=pdf_obj[0]
 
 pdf_obj.teyxos='2089'
@@ -37,6 +37,6 @@ print(type(jsonobj))
 diction=json.loads(jsonobj)
 print(diction['output'])
 if(diction['output']):
-    response = client.create_tweet(text=diction['output']+' Τεύχος: '+('not' or pdf_obj.teyxos)+' Φύλλο: '+('not found ' or pdf_obj.fullo))
+    response = client.create_tweet(text=diction['output'])
 else:
     print('not tweeted')
