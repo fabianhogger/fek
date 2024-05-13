e029029c10e97c5b476b4fa3f35433d14e37ebe7
fabianhogger <draizfabian@gmail.com>
2024-05-13 20:40:24 +0300

    trivial class

--- manage_tweets.py
+++ manage_tweets.py
@@ -10,10 +10,10 @@ parameters=json.load(f)
 class Tweeter_post:
     def __init__(self):
     # Define your Twitter API credentials
-    self.consumer_key = parameters["consumer_key"]
-    self.consumer_secret =  parameters["consumer_key_secret"]
-    self.access_token =  parameters["access_token"]
-    self.access_token_secret =  parameters["access_token_secret"]
+        self.consumer_key = parameters["consumer_key"]
+        self.consumer_secret =  parameters["consumer_key_secret"]
+        self.access_token =  parameters["access_token"]
+        self.access_token_secret =  parameters["access_token_secret"]
 
 
 post=Tweeter_post()
@@ -24,7 +24,7 @@ client = tweepy.Client(consumer_key=post.consumer_key,
 # Replace the text with whatever you want to Tweet about
 
 fek_object=Fek_getter()
-pdf_obj=fek_object.get_fek()
+pdf_obj=fek_object.get_latest_fek()
 input=pdf_obj[0]
 
 pdf_obj.teyxos='2089'
@@ -36,6 +36,6 @@ print(type(jsonobj))
 diction=json.loads(jsonobj)
 print(diction['output'])
 if(diction['output']):
-    response = client.create_tweet(text=diction['output']+' Τεύχος: '+('not' or pdf_obj.teyxos)+' Φύλλο: '+('not found ' or pdf_obj.fullo))
+    response = client.create_tweet(text=diction['output'])
 else:
     print('not tweeted')
