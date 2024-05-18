306f106f0afa3e724045b45dafd76c411a01eec8
Fabian <32894026+fabianhogger@users.noreply.github.com>
2024-05-18 12:58:41 +0300

    sys args (#23)
    * setters & tweet update
    
    * schedule run
    
    * trivial class
    
    * trivial class
    
    * chnage
    
    * sys args

--- manage_tweets.py
+++ manage_tweets.py
@@ -24,19 +24,19 @@ client = tweepy.Client(consumer_key=post.consumer_key,
 # Replace the text with whatever you want to Tweet about
 
 
-fek_object=Fek_getter()
-pdf_obj=fek_object.get_latest_fek()
-input=pdf_obj[0]
-
-pdf_obj.teyxos='2089'
-pdf_obj.fullo='B'
-sum=Summerize()
-jsonobj=sum.summerize(input)
-print(jsonobj)
-print(type(jsonobj))
-diction=json.loads(jsonobj)
-print(diction['output'])
-if(diction['output']):
-    response = client.create_tweet(text=diction['output'])
-else:
-    print('not tweeted')
+if __name__=='__main__':
+    import sys
+    fek_object=Fek_getter()
+    pdf_obj=fek_object.get_fek(year= (int(sys.argv[1]) or 2024), teuxos= (int(sys.argv[2]) or 2), fullo= (int(sys.argv[3]) or 2820 ))
+    print(type(pdf_obj  ))
+    input=pdf_obj[0]   
+    sum=Summerize()
+    jsonobj=sum.summerize(input)
+    print(jsonobj)
+    print(type(jsonobj))
+    diction=json.loads(jsonobj)
+    print(diction['output'])
+    if(diction['output']):
+        response = client.create_tweet(text=diction['output'])
+    else:
+        print('not tweeted')
